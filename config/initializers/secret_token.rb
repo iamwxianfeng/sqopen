5c8cce9e434be369856684d3e63e81bc521ca15e
wxianfeng <wang.fl1429@gmail.com>
2012-03-12 23:34:53 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sqopen::Application.config.secret_token = 'be12d450f3a7d931770a7d3f6f87b43ec8a6cfbe7e47bbbe725a2df3b6686a0ddcf9e0f0fea17eb6cfba6f38c8478cbf536d84aab7388d7b4f0eae3d5fe04904'
